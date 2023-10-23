2e960f70c0546c07f2efb27c110420be7ccf5771
Jos3ph31 <123521766+Jos3ph31@users.noreply.github.com>
2023-10-23 17:43:08 -0600

    Add files via upload

--- tienda_v1/src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ tienda_v1/src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -6,24 +6,39 @@ import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
-    
-    //La anotación autowired crea un único objeto mientras se ejecuta la aplicación 
-    
+
     @Autowired
     private CategoriaDao categoriaDao;
-    
+
     @Override
-    public List<Categoria> getCategorias(boolean activos){
-        
-        var lista = categoriaDao.findAll(); //encontrar todos los datos de la lista de la base de datos
-        
-        if(activos){
-            lista.removeIf(e-> !e.isActivo());
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = categoriaDao.findAll();
+        if (activos) {
+            lista.removeIf(e -> !e.isActivo());
         }
-        
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
     }
 }
